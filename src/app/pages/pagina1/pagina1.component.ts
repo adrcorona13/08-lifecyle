903e6aefa16478de68b4e45512d96ca60bf4a038
Adrian Pineda <adrian.pc13@hotmail.com>
2023-02-25 23:13:03 -0600

    ejemplos

--- src/app/pages/pagina1/pagina1.component.ts
+++ src/app/pages/pagina1/pagina1.component.ts
@@ -9,36 +9,37 @@ export class Pagina1Component
   OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked,
   AfterViewInit, AfterViewChecked, OnDestroy {
 
-  constructor() {
-    console.log('constructor');
+    nombre: string = ""  
 
-  }
+    constructor() {
+      console.log('constructor');
+    }
 
-  ngOnInit(): void {
-    console.log('ngOnInit');
-  }
-  ngOnChanges(): void {
-    console.log('ngOnChanges');
-  }
-  ngDoCheck(): void {
-    console.log('ngDoCheck');
-  }
-  ngAfterContentInit(): void {
-    console.log('ngAfterContentInit');
-  }
-  ngAfterContentChecked(): void {
-    console.log('ngAfterContentChecked');
-  }
-  ngAfterViewInit(): void {
-    console.log('ngAfterViewInit');
-  }
-  ngAfterViewChecked(): void {
-    console.log('ngAfterViewChecked');
-  }
-  ngOnDestroy(): void {
-    console.log('ngOnDestroy');
-  }
+    ngOnInit(): void {
+      console.log('ngOnInit');
+    }
+    ngOnChanges(): void {
+      console.log('ngOnChanges');
+    }
+    ngDoCheck(): void {
+      console.log('ngDoCheck');
+    }
+    ngAfterContentInit(): void {
+      console.log('ngAfterContentInit');
+    }
+    ngAfterContentChecked(): void {
+      console.log('ngAfterContentChecked');
+    }
+    ngAfterViewInit(): void {
+      console.log('ngAfterViewInit');
+    }
+    ngAfterViewChecked(): void {
+      console.log('ngAfterViewChecked');
+    }
+    ngOnDestroy(): void {
+      console.log('ngOnDestroy');
+    }
 
-  
+    guardar(){}
 
 }
