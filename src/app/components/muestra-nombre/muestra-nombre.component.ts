6115bf21699e58751628ea1dcd3ead123728da01
Adrian Pineda <adrian.pc13@hotmail.com>
2023-02-25 23:29:02 -0600

    ngOnChanges

--- src/app/components/muestra-nombre/muestra-nombre.component.ts
+++ src/app/components/muestra-nombre/muestra-nombre.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-muestra-nombre',
   templateUrl: './muestra-nombre.component.html'
 })
-export class MuestraNombreComponent {
+export class MuestraNombreComponent implements OnChanges {
+  @Input() nombre: string = '';
 
+  ngOnChanges(changes: SimpleChanges): void {
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    //Add '${implements OnChanges}' to the class.
+    console.log(changes);
+    
+  }
 }
