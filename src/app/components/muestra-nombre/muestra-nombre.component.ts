23bc8f869c3a3501a79745eec22bc59fee6fd5fa
Adrian Pineda <adrian.pc13@hotmail.com>
2023-02-25 22:55:18 -0600

    agregar hooks

---
+++ src/app/components/muestra-nombre/muestra-nombre.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-muestra-nombre',
+  templateUrl: './muestra-nombre.component.html'
+})
+export class MuestraNombreComponent {
+
+}
---
+++ src/app/pages/pagina1/pagina1.component.ts
@@ -0,0 +1,44 @@
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina1',
+  templateUrl: './pagina1.component.html'
+})
+export class Pagina1Component
+  implements
+  OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked,
+  AfterViewInit, AfterViewChecked, OnDestroy {
+
+  constructor() {
+    console.log('constructor');
+
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+  ngOnChanges(): void {
+    console.log('ngOnChanges');
+  }
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
+
+  
+
+}
